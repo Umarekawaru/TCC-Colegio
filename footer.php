ca696748ad6fa934189aca89d887133ca9b71022
FZero7 <38415943+FZero7@users.noreply.github.com>
2021-11-13 20:07:18 -0300

    update queries de local e data

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
       <div class="d-flex container justify-content-between">
-        <h4>Todos os direitos reservados</h4>
+        <h4>Todos os direitos reservados <?php echo date("Y")?></h4>
         <p>Colegio Isais Alvez, Barbalho Salvador</p>
    		
       </div>
