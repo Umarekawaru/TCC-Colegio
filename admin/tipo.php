35e3b687f0ace18319486bd94a7ac610d32bc507
Umarekawaru <60050746+Umarekawaru@users.noreply.github.com>
2021-11-17 00:00:51 -0300

    Atualização besta

--- admin/tipo.php
+++ admin/tipo.php
@@ -6,13 +6,13 @@ $tipo = $_GET['tipo']??"cidade";
 
 
 if ($tipo == "cidade") {
-		$stmt = $conn->prepare("SELECT * FROM cidade ");
+		$stmt = $conn->prepare("SELECT * FROM cidade ORDER BY local");
 		$stmt->execute();
 		$re = $stmt->fetchALL(PDO::FETCH_ASSOC);
 		$editar = "cidade-update.php?";
 		$delete = "../config/cidades/delete-cidadealta.php?";
 }else{
-		$stmt = $conn->prepare("SELECT * FROM local ");
+		$stmt = $conn->prepare("SELECT * FROM local ORDER BY local");
 		$stmt->execute();
 		$re = $stmt->fetchALL(PDO::FETCH_ASSOC);
 		$editar = "muse-update.php?";
